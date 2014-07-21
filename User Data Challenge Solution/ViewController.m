e5741616402b307fe13676cd449efc52422e60d5
Gauthier Delmee <gauthier.delmee@cyarasolutions.com>
2014-07-21 22:02:02 +1000

    Initial Commit

---
+++ User Data Challenge Solution/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  User Data Challenge Solution
+//
+//  Created by Gauthier Delmee on 21/07/2014.
+//  Copyright (c) 2014 Expectancy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+            
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ User Data Challenge Solution/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  User Data Challenge Solution
+//
+//  Created by Gauthier Delmee on 21/07/2014.
+//  Copyright (c) 2014 Expectancy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
