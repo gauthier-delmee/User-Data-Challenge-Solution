ef99cf64393d97cf361c35c724e98a5eec21b08a
Gauthier Delmee <gauthier.delmee@cyarasolutions.com>
2014-07-24 21:26:19 +1000

    added UITableView and prototype cells and filled in the cells with user data

--- User Data Challenge Solution/ViewController.h
+++ User Data Challenge Solution/ViewController.h
@@ -9,7 +9,9 @@
 #import <UIKit/UIKit.h>
 #import "CCUserData.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
 
 @property (strong, nonatomic) NSArray *users;
 
--- User Data Challenge Solution/ViewController.m
+++ User Data Challenge Solution/ViewController.m
@@ -20,15 +20,37 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     self.users = [CCUserData users];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
     
-    for (NSDictionary *userData in [CCUserData users]) {
-        NSLog(@"%@", userData);
-    }
+//    for (NSDictionary *userData in [CCUserData users]) {
+//        NSLog(@"%@", userData);
+//    }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark UITableViewDataSource
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *CellIdentifier = @"userCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+//    CCUserData *user = [self.users objectAtIndex:indexPath.row];
+    
+    NSDictionary *user = self.users[indexPath.row];
+    cell.textLabel.text = user[USER_NAME];
+    cell.detailTextLabel.text = user[USER_EMAIL];
+    cell.imageView.image = user[USER_PROFILE_PICTURE];
+    
+    return cell;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [self.users count];
 }
 
 @end
